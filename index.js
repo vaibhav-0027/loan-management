cccda53d12b5e89ea13f3aaa87e68b2edae98e12
vaibhav-0027 <vaibhavdhingra10@gmail.com>
2021-09-10 17:03:39 +0530

    changed port to env

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.get('/*', (req, res) => {
     return res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+app.listen(process.env.PORT || port, () => {
+    console.log('Server is up on port ' + process.env.PORT || port)
 })
